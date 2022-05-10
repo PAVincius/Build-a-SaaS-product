60ded2b0d5df8dbaa0ee079bc5c5fea8d65ddc72
Vinícius Prates Araújo <vinicius.pratesaraujo@gmail.com>
2022-05-10 13:33:20 -0300

    premium content

--- saas_product/pages/[id].js
+++ saas_product/pages/[id].js
@@ -1,9 +1,27 @@
 import { supabase } from "../public/utils/supabase"
-
+import { useState, useEffect } from "react"
+ 
 const LessonDetails = ({lesson}) => {
+  const [videoUrl, setVideoUrl] = useState()
+
+  const getPremiumContent = async () => {
+    const { data } = await supabase
+    .from('premium_content')
+    .select('video_url')
+    .eq("id", lesson.id)
+    .single()
+
+    setVideoUrl(data?.video_url)
+  }
+
+  useEffect(() => {
+    getPremiumContent();
+  },[])
+
     return (
         <div className="relative flex min-h-screen flex-col justify-center bg-gradient-to-r from-rose-100 to-teal-100">
             {lesson.title}
+            <p>{videoUrl}</p>
         </div>
     )
 }
